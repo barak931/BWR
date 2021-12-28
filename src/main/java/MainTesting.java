bac6dcf7bba475ffe63e7bd9889bde473d3f387b
Barak <barak.l@datarails.io>
2021-12-28 21:41:47 +0200

    Trying to solve a small issue on home pc

--- src/main/java/MainTesting.java
+++ src/main/java/MainTesting.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import report.htmlReport.Report;
 import rest.RestResponse;
 import robot.Robots;
 import tasks.Tasks;
@@ -14,12 +15,15 @@ public class MainTesting {
     private Robots robots;
     private Tasks tasks;
     private SoftAssert softAssert;
+    private Report report;
+
     @BeforeClass
     public void setup(){
         logger.info("Setup test function");
         robots = new Robots();
         tasks = new Tasks();
         logger.info(robots.getRobot(0).getRobotName());
+        report = new Report();
     }
 
     @Test(priority = 1)
@@ -27,6 +31,7 @@ public class MainTesting {
         logger.info("This is the first test -- This test will pass");
         RestResponse response = robots.getRobot(0).assignCommand(tasks.getTaskByIndex(1),true);
         softAssert.assertEquals(response.getStatusCode(),200);
+        report.reportTestResult("Assign command positive","Assign command to Robot 1 -- Pass", true);
     }
 
     @Test(priority = 2)
@@ -34,11 +39,13 @@ public class MainTesting {
         logger.info("Second test -- This test will fail");
         RestResponse response = robots.getRobot(0).assignCommand(tasks.getTaskByIndex(3),false);
         softAssert.assertEquals(response.getStatusCode(),200);
+        report.reportTestResult("Assign command negative","Assign command to Robot 1 -- Fail", false);
     }
 
     @AfterClass
     private void tearDown(){
-
+        softAssert.assertAll();
+        report.flush();
     }
 
 
--- src/main/java/report/htmlReport/Report.java
+++ src/main/java/report/htmlReport/Report.java
@@ -1,11 +1,29 @@
 package report.htmlReport;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.relevantcodes.extentreports.NetworkMode;
+
+import java.io.File;
+
 public class Report {
 
-    public Report(){
+    ExtentReports extentReports;
+    ExtentTest extentTest;
 
+    public Report(){
+        extentReports = new ExtentReports(System.getProperty("user.dir")+"/Report.html", NetworkMode.ONLINE);
+        extentReports.loadConfig(new File(Report.class.getResource("/extent-config.xml").getFile()));
     }
 
+    public void reportTestResult(String testName,String details, boolean pass){
+        extentTest = extentReports.startTest(testName);
+        extentTest.log(pass?LogStatus.PASS:LogStatus.FAIL,testName,details);
+        extentReports.endTest(extentTest);
+    }
 
-
+    public void flush(){
+        extentReports.flush();
+    }
 }
